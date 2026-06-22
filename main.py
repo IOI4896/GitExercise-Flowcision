29045b71e71345ce618cb50eeada07c1ba304605
Zi Yan <zn1691870@gmail.com>
2026-06-23 07:56:12 +0800

    testing

--- main.py
+++ main.py
@@ -1109,10 +1109,6 @@ def result():
         plot_url = plot_url
     )
 
-@app.errorhandler(404)
-def page_not_found(e):
-    
-    return render_template('404.html'), 404
 
 
 if __name__ == "__main__":
